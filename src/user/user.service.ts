1b2b5b03f395879dfed3fde0004697fe082239f3
philyas <lephi@gmx.net>
2024-07-02 18:04:36 +0200

    repository orm

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    console.log(process.env.DB_NAME)
-    return `This action returns all user`;
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: number): Promise<User> {
+    return this.usersRepository.findOne({where:{ id }});
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
