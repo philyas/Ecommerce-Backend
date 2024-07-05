d834a924e856dcd2920f5ce3ef72df6099943105
philyas <lephi@gmx.net>
2024-07-05 14:51:00 +0200

    fix config rabbitmq

--- src/rabbitmq/rabbitmq-client.module.ts
+++ src/rabbitmq/rabbitmq-client.module.ts
@@ -8,7 +8,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         name: 'rabbit@7c830cb07675',
         transport: Transport.RMQ,
         options: {
-          urls: ['amqp://localhost:15672'], // Adjust URL based on your RabbitMQ setup
+          urls: ['amqp://localhost:5672'], // Adjust URL based on your RabbitMQ setup
           queue: 'orders_queue', // Queue name
           queueOptions: {
             durable: true, // Make the queue durable
